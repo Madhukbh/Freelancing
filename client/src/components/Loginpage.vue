1ac2c01969e0391c0d2438820caaa9bb1f6c5fcb
Bhanu Teja Adapa <bhanuteja.a>
2020-02-12 20:20:45 +0530

    Initial commit

---
+++ client/src/components/Loginpage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>login page</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
